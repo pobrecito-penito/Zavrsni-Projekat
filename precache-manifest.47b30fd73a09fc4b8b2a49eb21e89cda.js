5c42bfcc7af11c77855f7893c49204c7faf01406
pobrecito-penito <jelenatmf197@gmail.com>
2020-04-03 11:23:29 +0200

    Updates

---
+++ precache-manifest.47b30fd73a09fc4b8b2a49eb21e89cda.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af8d52c2e9a90a86b331185fb34f006",
+    "url": "/Zavrsni-Projekat/final-project/index.html"
+  },
+  {
+    "revision": "52db67984cd2e5197959",
+    "url": "/Zavrsni-Projekat/final-project/static/css/main.ecea2165.chunk.css"
+  },
+  {
+    "revision": "d6cee148a99744f9698d",
+    "url": "/Zavrsni-Projekat/final-project/static/js/2.bbd06298.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Zavrsni-Projekat/final-project/static/js/2.bbd06298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52db67984cd2e5197959",
+    "url": "/Zavrsni-Projekat/final-project/static/js/main.88ce4ab2.chunk.js"
+  },
+  {
+    "revision": "5855f135c9179f1222bd",
+    "url": "/Zavrsni-Projekat/final-project/static/js/runtime-main.7a199ef3.js"
+  },
+  {
+    "revision": "0ad60f4a144af54fe3a48047921d9882",
+    "url": "/Zavrsni-Projekat/final-project/static/media/cat.0ad60f4a.gif"
+  },
+  {
+    "revision": "9c95f9afbb4e69ee423b2d62d90e8d9f",
+    "url": "/Zavrsni-Projekat/final-project/static/media/cat1.9c95f9af.gif"
+  },
+  {
+    "revision": "395f317d8e79f9c8ca7d1e9244f927e0",
+    "url": "/Zavrsni-Projekat/final-project/static/media/cats.395f317d.gif"
+  },
+  {
+    "revision": "f7383879788d7b228ba9d462113a1ee2",
+    "url": "/Zavrsni-Projekat/final-project/static/media/pokusaj7.f7383879.jpg"
+  },
+  {
+    "revision": "0cc38be66027873f0c5b8f8f868d7895",
+    "url": "/Zavrsni-Projekat/final-project/static/media/textBox.0cc38be6.png"
+  }
+]);
